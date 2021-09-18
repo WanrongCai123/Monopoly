4460da809bc7e2499e8cdc54aa8cb3e0a20c1b28
YilaiYan <yanyilai201708@gmail.com>
2021-09-18 11:35:59 -0500

    js function added

--- monopoly.js
+++ monopoly.js
@@ -1,3 +1,28 @@
-window.onload(){
+//initialize game and set variable
+function Game(){
+    var energy;
+    var intelligence;
+    var happiness;
+    var location
+    var image;
+
+    var player = function(){
+        energy = 10;
+        intelligence = 2;
+        happiness = 10;
+        image = document.getElementById();
+    }
+
+}
+//roll dice. Every time move forward, roll dice
+function rollDice(){
+
+}
+
+function getDice(){
+
+}
+
+function resetDice(){
 
 }
