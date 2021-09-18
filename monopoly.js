78f3d904171e098c5ea0a7b4193f3f2212532389
WanrongCai123 <wc52@rice.edu>
2021-09-18 13:54:51 -0500

    building update

--- monopoly.js
+++ monopoly.js
@@ -39,7 +39,7 @@ window.onload = function() {
     var intelligence;
     var happiness;
     var playerLoc;
-    var buld = [{x:600, y:300}, {x:550, y:400}, {x:750, y:400}, {x:450, y:500}, {x:400, y:600}, {x:350, y:500}, {x:250, y:300}, {x:100, y:300}, {x:300, y:320}, {x:320, y:320}, {x:320, y:350}, {x:400, y:320}, {x:340, y:150}, {x:550, y:270}, {x:570, y:300}, {x:600, y:200}, {x:550, y:700}];
+    var buildingLoc = [{x:600, y:300}, {x:550, y:400}, {x:750, y:400}, {x:450, y:500}, {x:400, y:600}, {x:350, y:500}, {x:250, y:300}, {x:100, y:300}, {x:300, y:320}, {x:320, y:320}, {x:320, y:350}, {x:400, y:320}, {x:340, y:150}, {x:550, y:270}, {x:570, y:300}, {x:600, y:200}, {x:550, y:700}];
     app = createBkgroundCanvas(document.getElementById("canvas1"), buld);
     playerCanvas = createPlayerCanvas(document.getElementById("canvas2"));
     console.log("2");
